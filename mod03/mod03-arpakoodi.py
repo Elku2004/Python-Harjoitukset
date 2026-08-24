948d6e3c60ca68ae9f09078a49ade47cc1c07268
Elias Lahtinen <elias.lahtinen10@gmail.com>
2026-08-25 01:12:48 +0300

    w1 tasks done

--- mod03/mod03-arpakoodi.py
+++ mod03/mod03-arpakoodi.py
@@ -1,4 +1,5 @@
 import random
 numero = random.randint(1,9)
 
-print(f"Nelinumeroinen koodi: \n{random.randint(1,9)}{random.randint(1,9)}{random.randint(1,9)}{random.randint(1,9)}")
+print(f"Kolminumeroinen koodi:\n{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}")
+print(f"Nelinumeroinen koodi:\n{random.randint(1,6)}{random.randint(1,6)}{random.randint(1,6)}{random.randint(1,6)}")
